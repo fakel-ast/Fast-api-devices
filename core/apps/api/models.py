5dbfb2eb6f5855b3aca67dcb879ce111ddecd221
fakel_ast <lvov-styopa@ya.ru>
2022-05-18 22:37:38 +0300

    Added models

--- core/apps/api/models.py
+++ core/apps/api/models.py
@@ -2,10 +2,16 @@ from tortoise.models import Model
 from tortoise import fields
 
 
-class Male(Model):
-    """Model for user male"""
-    title = fields.CharField(max_length=16)
+class Devices(Model):
+    """Model for device"""
+    dev_id = fields.CharField(max_length=200, index=True)
+    dev_type = fields.CharField(max_length=120, index=True)
 
+
+class Endpoint(Model):
+    """Model for endpoint"""
+    device = fields.ForeignKeyField('models.Devices', on_delete=fields.CASCADE)
+    comment = fields.TextField(null=True)
 
 
 
---
+++ core/config/tortoise_conf.py
@@ -0,0 +1,11 @@
+from core.config.settings import DATABASE_URI, APPS_MODELS
+
+TORTOISE_ORM = {
+    "connections": {"default": DATABASE_URI},
+    "apps": {
+        "models": {
+            "models": APPS_MODELS,
+            "default_connection": "default",
+        }
+    },
+}
